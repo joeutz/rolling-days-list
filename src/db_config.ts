5ea3066e2b60d10c2dd9a8cb71d3d6f1a0b3b7df
Joe Utz <joeutz@joes-mbp.attlocal.net>
2022-08-05 13:59:39 -0400

    updated migration and swagger config.

--- src/db_config.ts
+++ src/db_config.ts
@@ -3,9 +3,9 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 export const dataConfig = {
   type: 'postgres',
   host: 'localhost',
-  port: 55000,
-  username: 'postgres',
-  password: 'postgrespw',
+  port: 5432,
+  username: 'joe_admin',
+  password: 'jku_admin',
   database: 'rolling_days',
   entities: ['dist/**/*.entity{ .ts,.js}'],
   synchronize: false,
--- src/migrations/1659381425522-CreateTableTasks.ts
+++ src/migrations/1659381425522-CreateTableTasks.ts
@@ -3,13 +3,14 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateTableTasks1659381425522 implements MigrationInterface {
   name = 'CreateTableTasks1659381425522';
 
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE "task" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "description" character varying NOT NULL, "assignmentDate" TIMESTAMP NOT NULL, "status" "public"."task_status_enum" NOT NULL DEFAULT 'active', CONSTRAINT "PK_fb213f79ee45060ba925ecd576e" PRIMARY KEY ("id"))`,
-    );
-  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TYPE "public"."task_status_enum" AS ENUM('active', 'complete')`);
+    await queryRunner.query(`CREATE TABLE "task" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "description" character varying NOT NULL, "assignmentDate" TIMESTAMP NOT NULL, "status" "public"."task_status_enum" NOT NULL DEFAULT 'active', CONSTRAINT "PK_fb213f79ee45060ba925ecd576e" PRIMARY KEY ("id"))`);
+}
+
+public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE "task"`);
-  }
+    await queryRunner.query(`DROP TYPE "public"."task_status_enum"`);
+}
 }
